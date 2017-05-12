2814443d6fc22dd042b7b7ed66cc6f539dd7d950
SeedBoot <oliboots@gmail.com>
2017-05-12 16:47:53 +0100

    hmm

--- script/script.js
+++ script/script.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
-	
+
   $('body , .nav1 , .nav').hide();
-	
+
   $('body').fadeIn(600);
   
-  $('.ham-menu').on('click', function() {
+  $('.nav1').toggle($('.ham-menu').is(':hidden'));
+  
+  $('.ham-menu').on('click', function () {
     $('.nav1').slideToggle(400);
     $('.nav').slideUp(400);
   });
-  
-  $('.sub').on('click', function() {
+
+  $('.sub').on('click', function () {
     $(this).parent().parent().next().slideDown(400);
   });
-  
-  $('.back').on('click', function() {
+
+  $('.back').on('click', function () {
     $(this).parent().parent().slideUp(400);
     $(this).parent().parent().nextAll().slideUp(400);
   });
   
+  
+
 });
+
