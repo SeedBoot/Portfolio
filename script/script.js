c67d35c6bddd5f091c3578a486f98d02a146340b
SeedBoot <oliboots@gmail.com>
2017-05-17 16:27:51 +0100

    little updoots

--- script/script.js
+++ script/script.js
@@ -3,9 +3,23 @@ $(document).ready(function () {
   $('.mob-menu').on('click', function () {
     $('.nav').slideToggle(400);
   });
-  
+
   $('#webdevlink').on('click', function () {
-    window.location = ('projects/webdev-portfolio.html');
+    $(this).fadeTo(400, 0.01, function () {
+      window.location = ('webdev-portfolio.html');
+    });
+  });
+
+  $('#photolink').on('click', function () {
+    $(this).fadeTo(400, 0.01, function () {
+      window.location = ('photo-portfolio.html');
+    });
+  });
+
+  $('#graphiclink').on('click', function () {
+    $(this).fadeTo(400, 0.01, function () {
+      window.location = ('graphic-portfolio.html');
+    });
   });
 
 });
