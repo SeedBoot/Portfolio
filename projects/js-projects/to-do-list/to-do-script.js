9796cf5bd0b472a3ff026148b89ff3480a0b8339
SeedBoot <oliboots@gmail.com>
2017-06-02 16:51:03 +0100

    remaking to do list

--- projects/js-projects/to-do-list/to-do-script.js
+++ projects/js-projects/to-do-list/to-do-script.js
@@ -1,9 +1,18 @@
 var doField = document.querySelector('.dofield');
 var doSubmit = document.querySelector('.dosubmit');
-
 var deleteButton = document.querySelector('.delbtn');
 
 doField.focus();
+
+var listItems = document.getElementsByTagName('li');
+var i;
+for (i = 0; i < listItems.length; i++) {
+  var checkbox = document.createElement('input');
+  checkbox.type = 'button';
+  checkbox.className = 'chkbtn';
+  checkbox.value = '\u2714';
+  listItems[i].appendChild(checkbox);
+}
 
 function submitItem() {
   var userItem = String(doField.value);
@@ -13,28 +22,34 @@ function submitItem() {
     var li = document.createElement('LI');
 
     var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+    checkbox.type = 'button';
     checkbox.className = 'chkbtn';
+    checkbox.value = '\u2714';
+    
+    var spanInput = document.createElement('span');
+    spanInput.className = 'spaninput';
 
     var delBtn = document.createElement('input');
     delBtn.type = 'button';
     delBtn.className = 'delbtn';
-    delBtn.value = 'delete';
+    delBtn.value = 'X';
     delBtn.addEventListener('click', function (e) {
       li.parentNode.removeChild(li);
       doField.focus();
     }, false);
 
     list.appendChild(li);
+    
+    li.appendChild(spanInput);
+    spanInput.appendChild(document.createTextNode(userItem));
     li.appendChild(checkbox);
-    li.appendChild(document.createTextNode(userItem));
     li.appendChild(delBtn);
 
     doField.value = '';
     doField.focus();
 
   } else {
-    alert("Please insert a value!");
+    alert("Oi! Write your shopping list!");
   }
 }
 
