8bb34e45eda4a4e14c7893480b64e884d40b0171
SeedBoot <oliboots@gmail.com>
2017-06-04 19:10:01 +0100

    building...

--- projects/js-projects/to-do-list/to-do-script.js
+++ projects/js-projects/to-do-list/to-do-script.js
@@ -1,56 +1,96 @@
 var doField = document.querySelector('.dofield');
 var doSubmit = document.querySelector('.dosubmit');
-var deleteButton = document.querySelector('.delbtn');
+var listItems = document.getElementsByTagName('li');
+
+var close = document.getElementsByClassName('delbtn');
+var complete = document.getElementsByClassName('chkbtn');
+var spanny = document.getElementsByClassName('spaninput');
+
+var i;
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
 
 doField.focus();
-
-var listItems = document.getElementsByTagName('li');
-var i;
-for (i = 0; i < listItems.length; i++) {
-  var checkbox = document.createElement('input');
-  checkbox.type = 'button';
-  checkbox.className = 'chkbtn';
-  checkbox.value = '\u2714';
-  listItems[i].appendChild(checkbox);
-}
 
 function submitItem() {
   var userItem = String(doField.value);
 
-  if (userItem != "") {
+  if (userItem !== "") {
     var list = document.getElementById('list');
     var li = document.createElement('LI');
+
+    var spanning = document.createElement('span');
+    spanning.className = 'spanning';
+
+    var spanInput = document.createElement('span');
+    spanInput.className = 'spaninput';
 
     var checkbox = document.createElement('input');
     checkbox.type = 'button';
     checkbox.className = 'chkbtn';
     checkbox.value = '\u2714';
-    
-    var spanInput = document.createElement('span');
-    spanInput.className = 'spaninput';
+    checkbox.addEventListener('click', function() { 
+      var boldy = this.parentElement.children;
+      ///////WTF I JUST WANT TO STRIKETHROUGH////////
+      //if (hasClass(boldy, 'strike') !== true) {
+      //  boldy.classList.removeAttribute('strike')
+      //} else {
+        boldy.classList.add('strike');
+      });
+    //});
+    /*  
+      if(boldy.hasClass('strike') !== true) {
+        for (i = 0; i < boldy.length; i++) {
+          boldy[i].classList.add('strike')
+        }
+      }
+    });*/
 
     var delBtn = document.createElement('input');
     delBtn.type = 'button';
     delBtn.className = 'delbtn';
-    delBtn.value = 'X';
+    delBtn.value = '\u00D7';
     delBtn.addEventListener('click', function (e) {
-      li.parentNode.removeChild(li);
-      doField.focus();
+      var listy = this.parentElement;
+      listy.style.display = 'none';
     }, false);
 
     list.appendChild(li);
-    
-    li.appendChild(spanInput);
+    li.appendChild(spanning);
+
+    spanning.appendChild(spanInput);
     spanInput.appendChild(document.createTextNode(userItem));
-    li.appendChild(checkbox);
-    li.appendChild(delBtn);
+
+    spanning.appendChild(checkbox);
+    spanning.appendChild(delBtn);
 
     doField.value = '';
     doField.focus();
-
   } else {
     alert("Oi! Write your shopping list!");
+    doField.focus();
   }
 }
 
 doSubmit.addEventListener('click', submitItem);
+
+/*
+function strikethrough() {
+//Completes items in list
+for (i = 0; i < spanny.length; i++) {
+  spanny[i].style.textDecoration = "line-through";
+}
+};
+    
+//    .onclick = function() {
+//    alert('hello');
+//    var strike = document.getElementsByClassName('strike');
+//    style.setProperty("text-decoration", "line-through");
+//  }
+//};
+
+var complete2 = document.querySelector('chkbtn');
+complete2.addEventListener('click', strikethrough);
+*/
