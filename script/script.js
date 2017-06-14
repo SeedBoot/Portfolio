c3642597b255fcf779cb5fc64bc2c50a9b9faae3
SeedBoot <oliboots@gmail.com>
2017-06-14 16:57:36 +0100

    further

--- script/script.js
+++ script/script.js
@@ -1,32 +1,5 @@
 $(document).ready(function () {
-  //INDEX TILES//
   $('.mob-menu').on('click', function () {
     $('.nav').slideToggle(400);
   });
-
-  $('#webdevlink').on('click', function () {
-    $(this).fadeTo(400, 0.01, function () {
-      window.location = ('webdev-portfolio.html');
-    });
-  });
-
-  $('#photolink').on('click', function () {
-    $(this).fadeTo(400, 0.01, function () {
-      window.location = ('other-portfolio.html');
-    });
-  });
-  
-  //WEBDEV TILES//
-  $('#todo').on('click', function () {
-    window.open('projects/to-do-list/to-do-index.html');
-  });
-  
-  $('#this-site').on('click', function () {
-    window.open('index.html');
-  });
-  
-  $('#jg-site').on('click', function () {
-    window.open('projects/jg/index.html');
-  });
-
 });
