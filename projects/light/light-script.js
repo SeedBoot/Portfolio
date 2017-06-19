b4fe06327e1d0df88519b2098bcbc2c6a1611401
SeedBoot <oliboots@gmail.com>
2017-06-19 15:36:11 +0100

    style a lightbulb

--- projects/light/light-script.js
+++ projects/light/light-script.js
@@ -1,5 +1,6 @@
 var bulb = document.getElementById('bulb');
 var switchBulb = document.getElementById('switch');
+var wall = document.getElementById('wall');
 
 switchBulb.addEventListener('click', turnOn);
 
@@ -7,12 +8,17 @@ function turnOn() {
     if (switchBulb.textContent === 'Aha!') {
         switchBulb.textContent = 'Hmm..';
         if(bulb.classList){
+            wall.classList.toggle('bgroundgetlit')
             bulb.classList.toggle('getlit')
+            bulb.classList.toggle('bulbCol')
+            
         }
     } else {
         switchBulb.textContent = 'Aha!';
         if(bulb.classList){
+            wall.classList.toggle('bgroundgetlit')
             bulb.classList.toggle('getlit')
+            bulb.classList.toggle('bulbCol')
         }
     }
 };
